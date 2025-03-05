5615f7ec50a556bbf9f64ec9345a4d422deaf92d
Amrutha <amrutha@intersmart.in>
2025-03-05 11:33:50 +0530

    pull from ajay

--- src/components/features/home/LifeAtIndel.js
+++ src/components/features/home/LifeAtIndel.js
@@ -1,5 +1,16 @@
 export default function LifeAtIndel() {
   return (
-    <div>LifeAtIndel</div>
+    <section className="w-full pt-[200px] pb-[128px] md:pt-[150px] md:pb-[100px] sm:pt-[100px] sm:pb-[80px]">
+      <div className="container">
+        <div className="flex flex-wrap">
+          <div className="w-[795px]">
+
+          </div>
+          <div className="w-[calc(100%-795px)] pl[75px] md:pl[50px] md:pl[30px]">
+              
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
