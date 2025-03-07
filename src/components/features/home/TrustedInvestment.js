d029b664a66ed59b462f513388f477bf7e221799
Amrutha <amrutha@intersmart.in>
2025-03-07 16:06:29 +0530

    trusted secction completed

--- src/components/features/home/TrustedInvestment.js
+++ src/components/features/home/TrustedInvestment.js
@@ -31,16 +31,16 @@ export default function TrustedInvestment() {
             <div className="text-sm-1 mb-[15px] xl:mb-[20px]">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
             </div>
-            <ul className="list-disc pl-[10px] space-y-1 lg:space-y-2 marker:text-red-600 marker:text-[20px] marker:content-['•']">
-              <li className="text-[#323232] pl-[15px] text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[20px]">Listed in NSE/BSE</li>
-              <li className="text-[#323232] pl-[15px] text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[20px]">Listed in stock market</li>
-              <li className="text-[#323232] pl-[15px] text-[14px] lg:text-[15px] xl:text-[16px] 2xl:text-[20px]">Company total revenue: 500cr +</li>
+            <ul>
+              <li className="2xl:text-[20px]  xl:text-[18px] lg:text-[15px] text-[14px]  text-[#323232] relative pl-[20px] before:content-[''] before:absolute before:top-[10px] before:left-0 before:rounded-full before:bg-base2 before:w-[8px] before:h-[8px] mb-[3px]" >Listed in NSE/BSE</li>
+              <li className="2xl:text-[20px]  xl:text-[18px] lg:text-[15px] text-[14px]  text-[#323232] relative pl-[20px] before:content-[''] before:absolute before:top-[10px] before:left-0 before:rounded-full before:bg-base2 before:w-[8px] before:h-[8px] mb-[3px]" >Listed in stock market</li>
+              <li className="2xl:text-[20px]  xl:text-[18px] lg:text-[15px] text-[14px]  text-[#323232] relative pl-[20px] before:content-[''] before:absolute before:top-[10px] before:left-0 before:rounded-full before:bg-base2 before:w-[8px] before:h-[8px] " >Company total revenue: 500cr +</li>
             </ul>
             <div className="flex flex-wrap gap-[10px] lg:gap-[15px] 2xl:gap-[20px] mt-[20px] md:mt-[25px] xl:mt-[35px] 2xl:mt-[50px]">
               <div>
                 <Link
                   href="/"
-                  className="btn btn-base2 min-w-[120px] lg:min-w-[160px] xl:min-w-[200px] 2xl:min-w-[227px]"
+                  className="btn btn-base2 min-w-[150px] lg:min-w-[170px] xl:min-w-[200px] 2xl:min-w-[227px]"
                 >
                   INVESTORS REPORT
                 </Link>
