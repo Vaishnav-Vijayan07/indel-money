92fc19e399bf3e3b1219bd5f1765a40b28acd3a1
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-04-24 12:46:17 +0530

    changed file names

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,18 +3,18 @@ import AboutFinacial from "../../components/features/about/AboutFinacial";
 import AboutSupermarket from "../../components/features/about/AboutSupermarket";
 import AboutMessage from "../../components/features/about/AboutMessage";
 import Accolades from "../../components/features/about/Accolades";
-import Indelvalues from "../../components/features/about/Indelvalues";
-import Investors from "../../components/features/about/Investors";
-import LifeIndel from "../../components/features/about/lifeIndel";
+import Indelvalues from "../../components/features/about/IndelValuesInfo";
+import Investors from "../../components/features/about/InvestorsInfo";
+import LifeIndel from "../../components/features/about/LifeIndelInfo";
 
 // MOBILE_VERSION
 import MobAboutFinacial from "../../components/features/about/MobAboutFinacial";
 import MobAboutSupermarket from "../../components/features/about/MobAboutSupermarket";
 import MobAboutMessage from "../../components/features/about/MobAboutMessage";
 import MobAccolades from "../../components/features/about/MobAccolades";
-import MobIndelvalues from "../../components/features/about/MobIndelvalues";
-import MobInvestors from "../../components/features/about/MobInvestors";
-import MobLifeIndel from "../../components/features/about/MobLifeIndel";
+import MobIndelvalues from "../../components/features/about/MobIndelValueBanner";
+import MobInvestors from "../../components/features/about/MobInvestorsInfo";
+import MobLifeIndel from "../../components/features/about/MobLifeIndelInfo";
 
 export default function About() {
   return (
