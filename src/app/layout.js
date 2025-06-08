779d97f7da395022a2a6378c6b1b1c297f9c0367
afsalmern <afsal@intersmart.in>
2025-06-08 22:41:41 +0530

    bug fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -69,7 +69,7 @@ export default function RootLayout({ children }) {
         <main className="flex-grow mt-[var(--header-y)]">{children}</main>
         <Footer />
         <FloatingButton />
-        <Toaster position="top-right" />
+        {/* <Toaster position="top-right" /> */}
       </body>
     </html>
   );
