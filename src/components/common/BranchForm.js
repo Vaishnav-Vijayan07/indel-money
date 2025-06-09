9181a82ae778356ba06abc659f7c064bea7eb750
Joshua-Intersmart <joshua@intersmart.in>
2025-06-09 19:10:47 +0530

    API's added
    API's added

--- src/components/common/BranchForm.js
+++ src/components/common/BranchForm.js
@@ -9,99 +9,114 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-  // } from "@/components/ui/select";
 } from "../../components/ui/select";
 
-// import { Button } from "@/components/ui/button";
 import { Button } from "../../components/ui/button";
+import api from "@/lib/api/axios";
 
 export default function BranchForm() {
+  const [states, setStates] = React.useState([]);
+  const [districts, setDistricts] = React.useState([]);
+  const [locations, setLocations] = React.useState([]);
+
+  const fetchStates = async () => {
+    try {
+      const { data } = await api.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/career/states`);
+      console.log('DATA', data);
+      if (data.success) {
+        setStates(data.data);
+      } else {
+        console.error("Failed to fetch states:", data.message);
+      }
+    } catch (error) {
+      console.log("Error fetching states:", error);
+    }
+  };
+
+  const fetchDistricts = async () => {
+    try {
+      const { data } = await api.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/career/districts`);
+      if (data.success) {
+        setDistricts(data.data);
+      } else {
+        console.error("Failed to fetch states:", data.message);
+      }
+    } catch (error) {
+      console.log("Error fetching states:", error);
+    }
+  };
+
+  const fetchLocations = async () => {
+    try {
+      const { data } = await api.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/career/locations`);
+      if (data.success) {
+        setLocations(data.data);
+      } else {
+        console.error("Failed to fetch states:", data.message);
+      }
+    } catch (error) {
+      console.log("Error fetching states:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    fetchStates();
+    fetchDistricts();
+    fetchLocations();
+  }, []);
+
   return (
     <form className="flex flex-wrap -mx-[10px] sm:-mx-[10px] 2xl:-mx-[15px]">
       {/* State */}
       <div className="w-1/2 md:w-1/5 p-[6px_10px] sm:p-[10px] 2xl:p-[15px]">
-        {/* <Select className="shadow-none outline-none border-none focus-within:shadow-none focus:shadow-none">
-                    <SelectTrigger className="w-[100%] bg-[#E7EFF9] text-[rgba(50,50,50,0.60)] rounded-[7px] 2xl:rounded-[10px] shadow-none outline-none border-none cursor-pointer focus-within:shadow-none">
-                        <SelectValue placeholder="Select State" />
-                    </SelectTrigger>
-                    <SelectContent className="bg-[#E7EFF9] text-[rgba(50,50,50,0.80)] shadow-lg border border-gray-300 rounded-md w-full">
-                        <SelectItem value="State 1">State 1</SelectItem>
-                        <SelectItem value="State 2">State 2</SelectItem>
-                        <SelectItem value="State 3">State 3</SelectItem>
-                    </SelectContent>
-                </Select> */}
         <Select>
           <SelectTrigger className="w-full bg-[#e7eff9] border-[#e7eff9]">
             <SelectValue placeholder="Select State" />
           </SelectTrigger>
           <SelectContent className="bg-[#e7eff9] border-[#e7eff9]">
-            <SelectItem value="State 1">State 1</SelectItem>
-            <SelectItem value="State 2">State 2</SelectItem>
-            <SelectItem value="State 3">State 3</SelectItem>
+            {states.map((state, index) => (
+              <SelectItem key={state?.id} value={state?.id}>
+                {state?.state_name}
+              </SelectItem>
+            ))}
           </SelectContent>
         </Select>
       </div>
 
       {/* District */}
       <div className="w-1/2 md:w-1/5 p-[6px_10px] sm:p-[10px] 2xl:p-[15px]">
-        {/* <Select>
-          <SelectTrigger className="w-[100%] bg-[#E7EFF9] text-[rgba(50,50,50,0.60)] rounded-[7px] 2xl:rounded-[10px] shadow-none outline-none border-none cursor-pointer">
-            <SelectValue placeholder="Select District" />
-          </SelectTrigger>
-          <SelectContent className="bg-[#E7EFF9] text-[rgba(50,50,50,0.80)] shadow-lg border border-gray-300 rounded-md w-full">
-            <SelectItem value="District 1">District 1</SelectItem>
-            <SelectItem value="District 2">District 2</SelectItem>
-            <SelectItem value="District 3">District 3</SelectItem>
-          </SelectContent>
-        </Select> */}
         <Select>
           <SelectTrigger className="w-full bg-[#e7eff9] border-[#e7eff9]">
             <SelectValue placeholder="Select District" />
           </SelectTrigger>
           <SelectContent className="bg-[#e7eff9] border-[#e7eff9]">
-            <SelectItem value="District 1">District 1</SelectItem>
-            <SelectItem value="District 2">District 2</SelectItem>
-            <SelectItem value="District 3">District 3</SelectItem>
+            {districts.map((district, index) => (
+              <SelectItem key={district?.id} value={district?.id}>
+                {district?.district_name}
+              </SelectItem>
+            ))}
           </SelectContent>
         </Select>
       </div>
 
       {/* Location */}
       <div className="w-1/2 md:w-1/5 p-[6px_10px] sm:p-[10px] 2xl:p-[15px]">
-        {/* <Select>
-          <SelectTrigger className="w-[100%] bg-[#E7EFF9] text-[rgba(50,50,50,0.60)] rounded-[7px] 2xl:rounded-[10px] shadow-none outline-none border-none cursor-pointer">
-            <SelectValue placeholder="Select Location" />
-          </SelectTrigger>
-          <SelectContent className="bg-[#E7EFF9] text-[rgba(50,50,50,0.80)] shadow-lg border border-gray-300 rounded-md w-full">
-            <SelectItem value="Location 1">Location 1</SelectItem>
-            <SelectItem value="Location 2">Location 2</SelectItem>
-            <SelectItem value="Location 3">Location 3</SelectItem>
-          </SelectContent>
-        </Select> */}
         <Select>
           <SelectTrigger className="w-full bg-[#e7eff9] border-[#e7eff9]">
             <SelectValue placeholder="Select Location" />
           </SelectTrigger>
           <SelectContent className="bg-[#e7eff9] border-[#e7eff9]">
-            <SelectItem value="Location 1">Location 1</SelectItem>
-            <SelectItem value="Location 2">Location 2</SelectItem>
-            <SelectItem value="Location 3">Location 3</SelectItem>
+            {locations.map((location, index) => (
+              <SelectItem key={location?.id} value={location?.location_name}>
+                {location?.location_name}
+              </SelectItem>
+            ))}
           </SelectContent>
         </Select>
       </div>
 
       {/* Distance */}
       <div className="w-1/2 md:w-1/5 p-[6px_10px] sm:p-[10px] 2xl:p-[15px]">
-        {/* <Select>
-          <SelectTrigger className="w-[100%] bg-[#E7EFF9] text-[rgba(50,50,50,0.60)] rounded-[7px] 2xl:rounded-[10px] shadow-none outline-none border-none cursor-pointer">
-            <SelectValue placeholder="Select Distance" />
-          </SelectTrigger>
-          <SelectContent className="bg-[#E7EFF9] text-[rgba(50,50,50,0.80)] shadow-lg border border-gray-300 rounded-md w-full">
-            <SelectItem value="10km">10km</SelectItem>
-            <SelectItem value="20km">20km</SelectItem>
-            <SelectItem value="30km">30km</SelectItem>
-          </SelectContent>
-        </Select> */}
         <Select>
           <SelectTrigger className="w-full bg-[#e7eff9] border-[#e7eff9]">
             <SelectValue placeholder="Select Distance" />
