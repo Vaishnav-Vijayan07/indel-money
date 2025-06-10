1e0f70ce9e534be151f76e1c7151747eac34e5bb
afsalmern <afsal@intersmart.in>
2025-06-10 17:52:56 +0530

    bug fix in history

--- src/app/history/page.js
+++ src/app/history/page.js
@@ -21,6 +21,8 @@ async function fetchHistoryData() {
 export default async function History() {
   const { contents, images, inceptions, error } = await fetchHistoryData();
 
+  console.log(contents, images, inceptions, error);
+
   if(!contents || !images || !inceptions) {
     return <div>Failed to fetch history data</div>;
   }
