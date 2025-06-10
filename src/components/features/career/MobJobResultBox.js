c098a12c172ae6befe6c02a756c860f674c9f2b7
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-06-10 20:11:51 +0530

    Refactor career components for improved readability and functionality
    - Simplified the FindJob component by removing redundant job location and job result data.
    - Enhanced JobLocationBox for better code clarity and structure.
    - Updated FindJobForm to synchronize form state with URL query parameters and improved validation schema.
    - Adjusted JobResultBox to dynamically link to job details based on job ID.
    - Improved MakeYourMove component with better prop logging and structure.
    - Enhanced MobJobResultBox to safely access job title and location.
    - Added a new CareerDetail page to display job details.

--- src/components/features/career/MobJobResultBox.js
+++ src/components/features/career/MobJobResultBox.js
@@ -3,10 +3,12 @@ import Link from "next/link";
 import JobResultAlert from "./JobResultAlert";
 
 export default function MobJobResultBox({ item }) {
+  console.log("MobJobResultBox item:", item);
+  
   return (
     <div className="group w-full h-auto bg-white rounded-[10px] p-[15px] 4xs:p-[20px] shadow-[0_0_15px_0_rgba(0,0,0,0.1)] transition-all duration-300 hover:bg-[#c7e0ff] hover:border-base1/05">
       <div className="text-[16px] font-bold leading-none text-black w-full block mb-[12px]">
-        {item.job_title}
+        {item?.job_title}
       </div>
       <div className="flex flex-wrap items-center gap-[6px_4px] 4xs:gap-[8px_6px] xs:gap-[10px]">
         <div>
@@ -17,7 +19,7 @@ export default function MobJobResultBox({ item }) {
               width={10}
               height={14}
             />
-            {item.location}
+            {item?.location?.location_name}
           </div>
         </div>
         <div>
