24c993053d1b40def6f9346cf3d1b1f5f4d69fb2
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-01 17:47:50 +0530

    Add lodash dependency and update footer component to fetch data from API

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-
 // import localFont from "next/font/local";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
@@ -7,7 +6,6 @@ import Footer from "../components/layout/footer/Footer";
 import FloatingButton from "@/components/common/FloatingButton";
 // import ToasterWrapper from "@/components/common/ToasterWrapper";
 import { Toaster } from "react-hot-toast";
-
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -22,7 +20,32 @@ const montserrat = Montserrat({
 //     "Indel Money is one leading gold loan provider in Kerala, India that provides instant, hassle-free digital gold loans with flexible repayment.",
 // };
 
-export default function RootLayout({ children }) {
+const fetchFooterData = async () => {
+  try {
+    const { data } = await api.get("/web/footer", {});
+    if (data.status === "success" && data.data) {
+      setFooterData(data.data.content);
+      setIcons(data.data.icons);
+    }
+  } catch (error) {
+    console.error("Error fetching footer data:", error);
+  }
+};
+
+export default async function RootLayout({ children }) {
+  let footerContent = null;
+  let footerIcons = [];
+
+  try {
+    const res = await api.get("/web/footer");
+    if (res.data.status === "success") {
+      footerContent = res.data.data.content;
+      footerIcons = res.data.data.icons;
+    }
+  } catch (error) {
+    console.error("Failed to fetch footer data", error);
+  }
+
   return (
     <html lang="en">
       {/* <body
@@ -31,7 +54,7 @@ export default function RootLayout({ children }) {
       <body className={`${montserrat.variable} font-montserrat min-h-screen flex flex-col antialiased`}>
         <Header />
         <main className="flex-grow mt-[var(--header-y)]">{children}</main>
-        <Footer />
+        <Footer content={footerContent} icons={footerIcons} />
         <FloatingButton />
         <Toaster position="top-right" />
         {/* <ToasterWrapper /> */}
