2e7ac71ab6af1b022731a4c8bdfaef4ceb07c918
afsalmern <afsal@intersmart.in>
2025-07-01 18:50:15 +0530

    bug fixes and pagination in gallery

--- src/app/gallery/[slug]/page.js
+++ src/app/gallery/[slug]/page.js
@@ -3,9 +3,9 @@
 import GalleryDetail from "@/components/features/gallery/GalleryDetail";
 import GallerySlider from "../../../components/features/gallery/GallerySlider";
 
-async function fetchData(slug) {
+async function fetchMoreGalleryItems(slug) {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/web/event?slug=${slug}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/web/more-events?slug=${slug}`, {
       cache: "no-store", // Ensure fresh data
     });
 
@@ -14,34 +14,31 @@ async function fetchData(slug) {
 
     if (result.status === "success") {
       return {
-        images: galleryData?.galleryItems,
+        galleryItems: galleryData?.galleryItems,
         error: null,
       };
     }
 
     return {
-      images: null,
+      galleryItems: null,
       error: result.message,
     };
   } catch (error) {
     return {
-      images: null,
+      galleryItems: null,
       error: "Failed to fetch gallery data",
     };
   }
 }
 
-export default async function GalleryDetailPage({ params }) {
+export default async function GalleryDetailPage({ params, searchParams }) {
   const { slug } = await params;
-  const { images, error } = await fetchData(slug);
+  const { galleryItems, error: moreError } = await fetchMoreGalleryItems(slug);
 
   return (
     <>
-      {/* Gallery contents */}
-      <GalleryDetail galleryItems={images} error={error} />
-
-      {/* GallerySlider contents */}
-      <GallerySlider />
+      <GalleryDetail slug={slug} />
+      <GallerySlider galleryItems={galleryItems} />
     </>
   );
 }
