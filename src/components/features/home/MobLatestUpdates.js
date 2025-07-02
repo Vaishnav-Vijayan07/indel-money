0964dc3dd2da05550c2df53157811734d84f8b30
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-02 10:14:33 +0530

    Refactor HomeMarquee and MobLatestUpdates components: streamline code by removing unnecessary console logs and enhancing alt text for images; update HomeClient to use lazy loading for improved performance

--- src/components/features/home/MobLatestUpdates.js
+++ src/components/features/home/MobLatestUpdates.js
@@ -142,7 +142,7 @@ export default function MobLatestUpdates({ sliderItems, sliderTitle, type = "blo
                   <div className="w-full h-[100px] rounded-[16px] overflow-hidden relative z-0 mb-[12px]">
                     <Image
                       src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${item?.image}`}
-                      alt={item?.alt}
+                      alt={item?.alt || "blog alt"}
                       fill
                       sizes="160px"
                       className="transition-transform duration-300 object-cover group-hover:scale-105"
