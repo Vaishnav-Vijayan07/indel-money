a7735cda5aabee26051f58da0e55f0482b90c5a6
afsalmern <afsal@intersmart.in>
2025-07-06 13:16:31 +0530

    bug fix in mobile gold rates

--- src/components/features/home/MobHeroBanner.js
+++ src/components/features/home/MobHeroBanner.js
@@ -12,9 +12,18 @@ import Link from "next/link";
 
 import "./Home.css";
 import EnquiryForm from "@/components/common/EnquiryForm";
+import { useEffect, useState } from "react";
 
 export default function MobHeroBanner({ heroBanner, initialData, announcement }) {
-  console.log("heroBanner", heroBanner);
+  const [goldLiveRate, setGoldLiveRate] = useState("0");
+
+  useEffect(() => {
+    // Simulate fetching a fake gold rate
+    const min = 6000;
+    const max = 8000;
+    const rand = min + Math.random() * (max - min);
+    setGoldLiveRate(rand.toFixed(0));
+  }, []);
   return (
     <section className="w-full block relative z-0 overflow-hidden">
       <Swiper
@@ -59,6 +68,7 @@ export default function MobHeroBanner({ heroBanner, initialData, announcement })
         announcementText={announcement}
         goldRateLabel={initialData?.pageContent?.gold_rate_label}
         goldRateIcon={initialData?.pageContent?.gold_rate_icon}
+        goldRate={goldLiveRate}
       />
       <HeroBannerEnquiry />
     </section>
