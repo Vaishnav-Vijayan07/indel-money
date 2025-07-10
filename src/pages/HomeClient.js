1db74359710e594efb9b8a1f6c6bf980320919c6
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-10 16:25:53 +0530

    testing

--- src/pages/HomeClient.js
+++ src/pages/HomeClient.js
@@ -88,12 +88,12 @@ export default function Home({ initialData, serviceBanner, banner, branchLocator
       </div>
 
       {/* Branch locator contents*/}
-      <div className="hidden sm:block" id="branch-locator">
+      {/* <div className="hidden sm:block" id="branch-locator">
         <BranchLocator pageContent={branchLocatorData} variant={"home"} useQueryParams={false} />
       </div>
       <div className="block sm:hidden" id="branch-locator">
         <MobBranchLocator pageContent={branchLocatorData} />
-      </div>
+      </div> */}
 
       {/* Life at Indel contents*/}
       <div className="hidden sm:block">
