6c0f16f92482f22509c427526c8b40e067917e76
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-10 16:54:03 +0530

    testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,64 +32,10 @@ export default async function RootLayout({ children }) {
       <body className={`${montserrat.variable} font-montserrat min-h-screen flex flex-col antialiased`}>
         <Header />
         <main className="flex-grow mt-[var(--header-y)]">{children}</main>
-        {/* <Footer content={footerContent} icons={footerIcons} /> */}
+        <Footer content={footerContent} icons={footerIcons} />
         <FloatingButton />
         <Toaster position="top-right" />
       </body>
     </html>
   );
 }
-
-// // import localFont from "next/font/local";
-// import { Montserrat } from "next/font/google";
-// import "./globals.css";
-// import Header from "../components/layout/header/Header";
-// import Footer from "../components/layout/footer/Footer";
-// import FloatingButton from "@/components/common/FloatingButton";
-// // import ToasterWrapper from "@/components/common/ToasterWrapper";
-// import { Toaster } from "react-hot-toast";
-// import api from "../lib/api/axios";
-
-// const montserrat = Montserrat({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500", "600", "700", "800", "900"],
-//   variable: "--font-montserrat",
-//   display: "swap",
-// });
-
-// // export const metadata = {
-// //   title: "Indel Money",
-// //   description:
-// //     "Indel Money is one leading gold loan provider in Kerala, India that provides instant, hassle-free digital gold loans with flexible repayment.",
-// // };
-
-// export default async function RootLayout({ children }) {
-//   let footerContent = null;
-//   let footerIcons = [];
-
-//   try {
-//     const res = await api.get("/web/footer");
-//     if (res.data.status === "success") {
-//       footerContent = res.data.data.content;
-//       footerIcons = res.data.data.icons;
-//     }
-//   } catch (error) {
-//     console.error("Failed to fetch footer data", error);
-//   }
-
-//   return (
-//     <html lang="en">
-//       {/* <body
-//         className={`${amino.variable} font-amino min-h-screen flex flex-col antialiased`}
-//       > */}
-//       <body className={`${montserrat.variable} font-montserrat min-h-screen flex flex-col antialiased`}>
-//         <Header />
-//         <main className="flex-grow mt-[var(--header-y)]">{children}</main>
-//         <Footer content={footerContent} icons={footerIcons} />
-//         <FloatingButton />
-//         <Toaster position="top-right" />
-//         {/* <ToasterWrapper /> */}
-//       </body>
-//     </html>
-//   );
-// }
