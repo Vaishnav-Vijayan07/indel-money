ae7f72d1a4a9b384f0c3cfa484e430f5fbaed7be
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-10 20:20:14 +0530

    added head

--- src/app/head.js
+++ src/app/head.js
@@ -1,8 +1,10 @@
 export default function Head() {
   return (
     <>
-      <link rel="preconnect" href="https://indelmoney.com" crossOrigin="" />
+      <link rel="preconnect" href="https://beta.indelmoney.com" crossOrigin="" />
       <link rel="preload" as="font" href="/media/99b7f73d5af7c3e2-s.woff2" type="font/woff2" crossOrigin="" />
+      <link rel="preload" as="style" href="/css/e2ad796c6400005a.css" />
+      <link rel="preload" as="style" href="/css/a3a748e1f1052924.css" />
     </>
   );
 }
