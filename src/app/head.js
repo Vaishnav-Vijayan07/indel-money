369fb8f29dae81468139bbbf65e069b231a4f507
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-10 17:23:18 +0530

    added robots.txt and sitemap

--- src/app/head.js
+++ src/app/head.js
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <link rel="preconnect" href="https://beta.indelmoney.com" crossOrigin="" />
+      <link rel="preconnect" href="https://indelmoney.com" crossOrigin="" />
       <link rel="preload" as="font" href="/media/99b7f73d5af7c3e2-s.woff2" type="font/woff2" crossOrigin="" />
     </>
   );
