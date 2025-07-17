00823474b21a411f78da946dc4ddec8e47117bd1
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-07-17 17:55:27 +0530

    fixed document issues

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -9,11 +9,19 @@ async function fetchManagementData() {
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/web/our-services`, {
       // cache: "no-store", // Ensure fresh data
-      cache: "force-cache",
-      next: { revalidate: 600 },
+      // cache: "force-cache",
+      // next: { revalidate: 600 },
     });
+
+    console.log("Fetching service data from:", `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/web/our-services`,
+      response
+    );
+    
     const result = await response.json();
     const serviceData = result.data;
+
+    console.log("Service Data:", serviceData);
+    
 
     if (result.status === "success") {
       return {
@@ -120,8 +128,14 @@ export async function generateMetadata() {
 export default async function Services() {
   const { serviceContent, services, serviceBenefit, error } = await fetchManagementData();
 
+  console.log("Service Content:", serviceContent);
+  console.log("Services:", services);
+  console.log("Service Benefit:", serviceBenefit);
+  console.log("Error:", error);
+  
+
   if ((!serviceContent && !services && !serviceBenefit) || error) {
-    return <div>Failed to fetch about data</div>;
+    return <div>Failed to fetch service data</div>;
   }
 
   return (
