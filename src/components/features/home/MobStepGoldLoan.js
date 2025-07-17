361b31bbc43450428efec9f610a18623d033e562
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-07-17 10:24:35 +0530

    Changed tags in mobile view

--- src/components/features/home/MobStepGoldLoan.js
+++ src/components/features/home/MobStepGoldLoan.js
@@ -5,7 +5,7 @@ export default function MobStepGoldLoan({ title, className, loanSteps }) {
   return (
     <section className={`${className} w-full py-[20px_0]`}>
       <div className="container">
-        <div className="text-title1 text-center mb-[10px] 2xl:mb-[15px] [&>span]:text-base2 [&>span]:font-bold">{title ? parse(title) : ""}</div>
+        <h2 className="text-title1 text-center mb-[10px] 2xl:mb-[15px] [&>span]:text-base2 [&>span]:font-bold">{title ? parse(title) : ""}</h2>
         <div className="flex flex-wrap">
           {loanSteps?.map((item, index) => (
             <div key={index} className="w-full py-[10px]">
@@ -14,9 +14,9 @@ export default function MobStepGoldLoan({ title, className, loanSteps }) {
                   <Image src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${item.icon_url}`} alt={"icon"} fill className="aspect-square" />
                 </div>
                 <div className="w-[calc(100%-80px)]">
-                  <div className="text-[13px] font-normal leading-none line-clamp-1 text-white text-center capitalize mb-[5px] @md:mb-[10px] bg-gradient-to-tl from-base2 to-base1 rounded-[15px] inline-block p-[8px_15px]">
+                  <h3 className="text-[13px] font-normal leading-none line-clamp-1 text-white text-center capitalize mb-[5px] @md:mb-[10px] bg-gradient-to-tl from-base2 to-base1 rounded-[15px] inline-block p-[8px_15px]">
                     {item?.title ? parse(item?.title) : ""}
-                  </div>
+                  </h3>
                   <div className="text-[14px] font-medium leading-none line-clamp-3 text-[#002362]">{item?.description}</div>
                 </div>
               </div>
