165825e1d69ee8a4bd2c97864a226b0de2748a28
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-07-22 15:16:05 +0530

    issue fixed - Experience added in career jobs

--- src/components/features/career/JobResultBox.js
+++ src/components/features/career/JobResultBox.js
@@ -24,7 +24,9 @@ export default function JobResultBox({ item, variant = "default" }) {
   const whatsappUrl = `https://wa.me/?text=${shareText}`;
   const emailUrl = `mailto:?subject=${encodeURIComponent("Job Opportunity: " + jobTitle)}&body=${shareText}`;
 
- 
+  console.log(item);
+
+
   return (
     <div
       className={`${
@@ -118,10 +120,11 @@ export default function JobResultBox({ item, variant = "default" }) {
           </div>
         </div>
         <div className="text-[12px] lg:text-[12px] xl:text-[14px] 2xl:text-[16px] 3xl:text-[18px] leading-none font-medium text-[#484877] mb-[5px] lg:mb-[10px] 2xl:mb-[20px]">
-          {variant === "default" && `Location: ${toSentenceCase(item?.location?.location_name)}`}
+          {variant === "default" && `Experience: ${item.experience}`}
           {variant === "activeJobs" && `Experience: ${item.experience}`}
         </div>
         <div className="text-[12px] lg:text-[12px] xl:text-[14px] 2xl:text-[16px] 3xl:text-[18px] leading-none font-medium text-[#484877] mb-[5px] lg:mb-[10px] 2xl:mb-[20px]">
+          {variant === "default" && `Location: ${toSentenceCase(item?.location?.location_name)}`}
           {variant === "activeJobs" && `Location: ${item?.location?.location_name}`}
         </div>
       </div>
