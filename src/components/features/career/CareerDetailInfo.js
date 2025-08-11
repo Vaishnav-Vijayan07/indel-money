1a9c6f3f320273f65f84af25deffaba02017a85a
Nikhil-Govind-Inter <nikhil@intersmart.in>
2025-08-11 16:12:49 +0530

    year issue fixed in bob details

--- src/components/features/career/CareerDetailInfo.js
+++ src/components/features/career/CareerDetailInfo.js
@@ -13,6 +13,7 @@ function CareerDetailInfoBox({ children }) {
 
  
 export default function CareerDetailInfo({ job }) {
+
   return (
     <section className="w-full h-auto py-[30px] lg:py-[50px_80px]">
       <div className="container">
@@ -28,7 +29,7 @@ export default function CareerDetailInfo({ job }) {
                 </div>
                 <div className="flex flex-wrap gap-[5px] sm:gap-[10px] lg:gap-[15px] 2xl:gap-[20px]">
                   <div>
-                    <CareerDetailInfoBox>Experience: {parseInt(job?.experience) || "Not specified"}</CareerDetailInfoBox>
+                    <CareerDetailInfoBox>Experience: {job?.experience || "Not specified"}</CareerDetailInfoBox>
                   </div>
                   <div>
                     <CareerDetailInfoBox>
