b80c2638f9f75cb494496ce5a2bd13eebef2b372
Vaishnav-Vijayan07 <vaishnav@intersmart.in>
2025-08-11 15:45:37 +0530

    feat: Add GTMTracker component for enhanced Google Tag Manager integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import FloatingButton from "../components/common/FloatingButton";
 import { Toaster } from "react-hot-toast";
 import api from "../lib/api/axios";
 import { GoogleTagManager } from "@next/third-parties/google";
+import GTMTracker from "./gtm-tracker";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -46,6 +47,7 @@ export default async function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+        <GTMTracker />
       </body>
     </html>
   );
